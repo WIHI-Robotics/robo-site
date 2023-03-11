b456731fe52eed8347b2bb31f6cbec16a32855bb
Ari Shtein <ashtein120@gmail.com>
2023-03-11 17:42:20 -0500

    Add /pages/about/competition.html using mostly ChatGPT (w/ edits)

--- resources/scripts/roboPicChanger.js
+++ resources/scripts/roboPicChanger.js
@@ -3,7 +3,7 @@ const roboPic2 = document.querySelector('img.robo-pic#robo-pic-2')
 const roboPic3 = document.querySelector('img.robo-pic#robo-pic-3')
 
 const setPics = () => {
-    if (window.outerWidth <= 700) {
+    if (window.outerWidth <= 720) {
         roboPic1.src = '/robo-site/resources/images/robot/mobile-1.jpg'
         roboPic2.src = '/robo-site/resources/images/robot/mobile-2.jpg'
         roboPic3.src = '/robo-site/resources/images/robot/mobile-3.jpg'
