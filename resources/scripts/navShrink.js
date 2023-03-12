6e8ef180dfcf18b9e378406c1f9b15a4aa2d78a1
Ari Shtein <ashtein120@gmail.com>
2023-03-12 16:43:00 -0400

    Add dark mode & color mode toggler

--- resources/scripts/navShrink.js
+++ resources/scripts/navShrink.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector('nav.navbar')
 const navLogoImage = document.querySelector('img#logo')
 const navMenuButton = document.querySelector('button.navbar-toggler')
 const navLinks = document.querySelectorAll('a.nav-link.shrinks')
+const colorMenuSVGs = document.querySelectorAll('svg.bi')
 
 navLinks.forEach(link => {
     link.classList.add('fs-5')
@@ -20,6 +21,9 @@ const shrinkNav = () => {
                 link.classList.add('fs-6')
             }
         })
+        colorMenuSVGs.forEach(svg => {
+            svg.style.transform = "scale(0.8)"
+        })
     } else {
         navMenuButton.style.transform = "none"
         navLogoImage.width = 60
@@ -31,6 +35,9 @@ const shrinkNav = () => {
                 link.classList.remove('fs-6')
                 link.classList.add('fs-5')
             }
+        })
+        colorMenuSVGs.forEach(svg => {
+            svg.style.transform = "none"
         })
     }
 }
